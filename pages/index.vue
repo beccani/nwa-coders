abdf2e96ddfb27c78d89c47759b677af648ac66f
xamthor <miguel@mhernandez.me>
2021-08-03 10:37:00 -0500

    ...

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,8 @@
 			<div class="row">
 				<div class="col-sm-8 offset-sm-2 text-gray-700 text-center -mt-52">
 					<div class="relative mb-8">
-						<h1 class="relative text-9xl tracking-tighter-less text-shadow font-sans font-bold">
-						<span>N</span><span>W</span><span>A</span> <br> <span>C</span> <span>O</span> <span>D</span>
-						<span>E</span><span>R</span><span>S</span></h1>
+						<h1 class="relative text-6xl md:text-9xl text-shadow font-sans font-bold">
+						<span>N</span><span>W</span><span>A</span><br><span>C</span><span>O</span><span>D</span><span>E</span><span>R</span><span>S</span></h1>
 					</div>
 					<p class="my-8 text-2xl">site coming soon</p>
 					<a class="bg-gray-800 px-5 py-3 text-sm shadow-sm font-medium tracking-wider text-gray-50 rounded-full hover:shadow-lg" href="https://discord.com/channels/830116083594952735/854164136932671502/870696629274038283"> Join Discord Community </a>
@@ -19,9 +18,6 @@
 
 
 <style>
-	.tracking-tighter-less {
-		letter-spacing: -0.75rem;
-	}
 	.text-shadow {
 		text-shadow: -8px 0 0 #e3e3e3;
 	}
