f6bc7eb2ed1e9d0fbe816e38446819984b7d381a
xamthor <miguel@mhernandez.me>
2021-08-11 10:22:07 -0500

    highlight event happening in current day

--- components/MeetSchedule.vue
+++ components/MeetSchedule.vue
@@ -1,18 +1,17 @@
 <template>
-	
 	<div>
-
 		<h2 class="text-2xl my-12 text-white bg-black w-max p-4 mx-auto">Meet-up Schedule</h2>
-		
 		<div v-for="item in data.results" :key="item.id">
-			<div class="relative block rounded-lg border border-gray-300 bg-white shadow-sm px-6 py-4 cursor-pointer hover:border-gray-400 sm:flex sm:justify-between focus:outline-none mb-4">
+			<div 
+			:class="{ 'border-blue-800 border-4': getDate(item.properties.Day.date.start) }"
+			 class="relative block rounded-lg border-2 border-gray-300 bg-white shadow-sm px-6 py-4 cursor-pointer hover:border-gray-400 sm:flex sm:justify-between focus:outline-none mb-4">
 				<div class="flex text-left">
 					<div class="text-sm">
 						<div class="font-medium text-gray-900">
 							{{item.properties.Name.title[0].plain_text}}
 						</div>
 						<div class="text-gray-500">
-							<p class="sm:inline">{{item.properties.Time.rich_text[0].plain_text}}</p>
+							<p class="sm:inline">{{item.properties.Time.rich_text[0].plain_text}} CST</p>
 						</div>
 					</div>
 				</div>
@@ -21,9 +20,7 @@
 				</div>
 			</div>
 		</div>
-
 	</div>
-
 </template>
 
 <script>
@@ -32,14 +29,21 @@
 
 	export default {
 
-		props: { data: Object },
+		props: { data: { type: Object, default: () => ({}) } },
 
 		methods:{
 			date(date){
+
+				if(new Date().getDate().toString() === dateFormat(date, "UTC:d")) return "Today"
 				return dateFormat(date, "UTC:mmmm dS, yyyy");
+			},
+			getDate(date){
+
+				console.log(new Date().getDate() +' '+ dateFormat(date, "UTC:d"))
+				console.log(new Date().getDate().toString() === dateFormat(date, "UTC:d"))
+				return new Date().getDate().toString() === dateFormat(date, "UTC:d");
 			}
 		}
 	}
-
 
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,42 +4,50 @@
 				<div class="col-sm-8 offset-sm-2 text-gray-700 text-center ">
 					<div class="relative mb-8">
 						<h1 class="relative text-6xl md:text-9xl text-shadow font-sans font-bold">
-						<span>N</span><span>W</span><span>A</span><br><span>C</span><span>O</span><span>D</span><span>E</span><span>R</span><span>S</span></h1>
+							<span v-for="letter in title" :key="letter">{{ letter }}</span><br><span v-for="letter in subTitle" :key="letter">{{ letter }}</span>
+						</h1>
 					</div>
 					<p class="my-8 text-2xl">site coming soon</p>
 					<a class="bg-gray-800 px-5 py-3 text-sm shadow-sm font-medium tracking-wider text-gray-50 rounded-full hover:shadow-lg" href="https://discord.com/channels/830116083594952735/854164136932671502/870696629274038283"> Join Discord Community </a>
-
 					<MeetSchedule :data="data"/>
-
+					
 				</div>
 			</div>
 		</div>
 </template>
 
+
 <script>
-export default {
+	export default {
 
-	async asyncData({ $axios }) {
-		
-		let data = {}
-
-		try{
-			
-			const filterData = { 
-				filter: { property: "Status", select: { equals: "Published" } },
-				sorts: [ { property: "Day", direction: "ascending" }]
+		data(){
+			return {
+				title: 'NWA',
+				subTitle: 'CODERS'
 			}
-			
-			data = await $axios.$post('databases/d9f93215943f4d6ea9e73ab3af2f569c/query', filterData)
+		},
 
-		}catch(err){
+		async asyncData({ $axios }) {
 			
+			let data = {}
+
+			try{
+				
+				const filterData = { 
+					filter: { property: "Status", select: { equals: "Published" } },
+					sorts: [ { property: "Day", direction: "ascending" }]
+				}
+				
+				data = await $axios.$post('databases/d9f93215943f4d6ea9e73ab3af2f569c/query', filterData)
+
+			}catch(err){
+				
+			}
+
+			return { data }
 		}
 
-		return { data }
 	}
-
-}
 </script>
 
 
